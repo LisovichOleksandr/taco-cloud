73993edffa7fa4fe8c3cac033bf4a41e43184748
Oleksandr Lisovich <lisovyhs@gmail.com>
2024-10-18 07:29:23 +0300

    Added and learned Converter class

--- src/main/java/ua/lisovich/tacos/converter/IngredientByIdConverter.java
+++ src/main/java/ua/lisovich/tacos/converter/IngredientByIdConverter.java
@@ -1,4 +1,40 @@
+/* Конвертер – это любой класс, который реализует интерфейс Converter
+с методом convert(), получающим значение одного типа и преоб-
+разующим его в значение другого типа.
+ "Spring in Action" - Craig Walls
+ 18.10.24
+ */
+
 package ua.lisovich.tacos.converter;
 
-public class IngredientByIdConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ua.lisovich.tacos.Ingredient;
+import ua.lisovich.tacos.Ingredient.Type;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private Map<String, Ingredient> ingredientMap = new HashMap<>();
+
+    public IngredientByIdConverter() {
+        ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+        ingredientMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+        ingredientMap.put("GRBF", new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+        ingredientMap.put("CARN", new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+        ingredientMap.put("TMTO", new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+        ingredientMap.put("LETC", new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+        ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
+        ingredientMap.put("JACK", new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+        ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
+        ingredientMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientMap.get(id);
+    }
 }
